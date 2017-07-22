09030c86f73ff5a02e89aeec6bd16fcc5f0dac50
James Anthony Bruno <j.bruno.che@gmail.com>
2017-07-22 11:32:34 -0500

    Improve iter time by precomputing pixel int values; close #2

--- src/acidity.cr
+++ src/acidity.cr
@@ -10,19 +10,6 @@ if ARGV.size != 2 && ARGV.size != 3
   exit(-1)
 end
 
-def rgba_to_int(rgba : RGBA)
-  r, g, b, a = rgba.to_rgba
-  return ((r.to_u64 << 24) + (g.to_u64 << 16) + (b.to_u64 << 8) + a.to_u64)
-end
-
-def int_to_rgba(i)
-  r = ((i >> 24) & 0xFF).to_u16
-  g = ((i >> 16) & 0xFF).to_u16
-  b = ((i >> 8) & 0xFF).to_u16
-  a = (i & 0xFF).to_u16
-  return RGBA.from_rgba8(r, g, b, a)
-end
-
 if ARGV.size == 3
   radius = ARGV[2].to_i
   if radius <= 0
@@ -33,19 +20,44 @@ else
   radius = 9
 end
 
+##
+# Converts StumpyPNG's RGBA format to int.
+def rgba_to_int(rgba : RGBA)
+  r, g, b, a = rgba.to_rgba
+  return ((r.to_u64 << 24) + (g.to_u64 << 16) + (b.to_u64 << 8) + a.to_u64)
+end
+
+##
+# Converts int format to StumpyPNG's RGBA format.
+def int_to_rgba(i)
+  r = ((i >> 24) & 0xFF).to_u16
+  g = ((i >> 16) & 0xFF).to_u16
+  b = ((i >> 8) & 0xFF).to_u16
+  a = (i & 0xFF).to_u16
+  return RGBA.from_rgba8(r, g, b, a)
+end
+
 input = StumpyPNG.read(ARGV[0])
 w, h = input.width, input.height
+int_canvas = Array(UInt64).new(w * h, (0).to_u64)
 output = Canvas.new(w, h)
 
-(0...w).each do |x|
-  (0...h).each do |y|
-    pxl = rgba_to_int(input[x, y])
-    count = 1
+# Precompute int values for pixels
+(0...h).each do |y|
+  (0...w).each do |x|
+    int_canvas[y * w + x] = rgba_to_int(input[x, y])
+  end
+end
+
+(0...h).each do |y|
+  (0...w).each do |x|
+    pxl = int_canvas[y * w + x]
+    count = 0
     ((x - radius)..(x + radius)).each do |xi|
       if xi < w && xi >= 0
         ((y - radius)..(y + radius)).each do |yi|
           if yi < h && yi >= 0 && (yi != y && xi != x)
-            pxl += rgba_to_int(input[xi, yi])
+            pxl += int_canvas[yi * w + xi]
             count += 1
           end
         end
