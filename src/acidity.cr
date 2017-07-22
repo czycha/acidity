f44dfafcfea3a82452fbda6c0c8856c947890094
James Anthony Bruno <j.bruno.che@gmail.com>
2017-07-22 11:00:35 -0500

    User input for radius; close #1

--- src/acidity.cr
+++ src/acidity.cr
@@ -2,47 +2,58 @@ require "./acidity/*"
 require "stumpy_png"
 include StumpyPNG
 
-if ARGV.size != 2
-	puts "Usage: acidity INPUT.png OUTPUT.png"
-	exit(-1)
+if ARGV.size != 2 && ARGV.size != 3
+  puts "Usage: acidity INPUT.png OUTPUT.png [RADIUS]
+       INPUT: Required. PNG image to convert.
+       OUTPUT: Required. PNG image to write to.
+       RADIUS: Optional, defaults to 9. Higher numbers takes longer, but produce larger bands of texture."
+  exit(-1)
 end
 
 def rgba_to_int(rgba : RGBA)
-	r, g, b, a = rgba.to_rgba
-	return ((r.to_u64 << 24) + (g.to_u64 << 16) + (b.to_u64 << 8) + a.to_u64)
+  r, g, b, a = rgba.to_rgba
+  return ((r.to_u64 << 24) + (g.to_u64 << 16) + (b.to_u64 << 8) + a.to_u64)
 end
 
 def int_to_rgba(i)
-	r = ((i >> 24) & 0xFF).to_u16
-	g = ((i >> 16) & 0xFF).to_u16
-	b = ((i >> 8) & 0xFF).to_u16
-	a = (i & 0xFF).to_u16
-	return RGBA.from_rgba8(r, g, b, a)
+  r = ((i >> 24) & 0xFF).to_u16
+  g = ((i >> 16) & 0xFF).to_u16
+  b = ((i >> 8) & 0xFF).to_u16
+  a = (i & 0xFF).to_u16
+  return RGBA.from_rgba8(r, g, b, a)
 end
 
-SPLIT = 9
+if ARGV.size == 3
+  radius = ARGV[2].to_i
+  if radius <= 0
+    puts "Invalid radius."
+    exit(-1)
+  end
+else
+  radius = 9
+end
 
 input = StumpyPNG.read(ARGV[0])
 w, h = input.width, input.height
 output = Canvas.new(w, h)
 
 (0...w).each do |x|
-	(0...h).each do |y|
-		pxl = rgba_to_int(input[x, y])
-		count = 1
-		((x - SPLIT)..(x + SPLIT)).each do |xi|
-			if xi < w && xi >= 0
-				((y - SPLIT)..(y + SPLIT)).each do |yi|
-					if yi < h && yi >= 0 && (yi != y && xi != x)
-						pxl += rgba_to_int(input[xi, yi])
-						count += 1
-					end
-				end
-			end
-		end
-		color = (pxl / count) | 255
-		output[x, y] = int_to_rgba(color)
-	end
+  (0...h).each do |y|
+    pxl = rgba_to_int(input[x, y])
+    count = 1
+    ((x - radius)..(x + radius)).each do |xi|
+      if xi < w && xi >= 0
+        ((y - radius)..(y + radius)).each do |yi|
+          if yi < h && yi >= 0 && (yi != y && xi != x)
+            pxl += rgba_to_int(input[xi, yi])
+            count += 1
+          end
+        end
+      end
+    end
+    color = (pxl / count) | 255
+    output[x, y] = int_to_rgba(color)
+  end
 end
 
 StumpyPNG.write(output, ARGV[1])
